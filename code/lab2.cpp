6c91e3eca5790bc8fdb52df7fb6219e238c0be84
qqqq4u <danila16102003@rambler.ru>
2021-09-13 20:16:50 +0300

    FINALLY REWORKED SCHEME

--- code/lab2.cpp
+++ code/lab2.cpp
@@ -2,66 +2,103 @@
 
 using namespace std;
 
-void dayTimeEdit(const char *tempDayTime, char dayTime[15]) {
-    for (int i = 0; i < sizeof(tempDayTime); i++) {
+void dayTimeEdit(const char *tempDayTime, char dayTime[15])
+{
+    for (int i = 0; i < sizeof(tempDayTime); i++)
+    {
         dayTime[i] = tempDayTime[i];
     }
 }
 
-int main() {
+int main()
+{
+    do
+    {
+        /* code */
 
-    int hours, minutes;
-    char dayTime[15] = "";
+        int hours, minutes;
+        char dayTime[15] = "";
         cout << "Введите часы и минуты" << endl;
         cin >> hours >> minutes;
 
         if (hours > 24 || minutes > 59) //проверка правильности вводимого времени
         {
             cout << "Введены недопустимые данные!";
-        } else {
+        }
+        else
+        {
             if (hours == 12 && minutes == 0) // частные случаи
             {
                 cout << "Полдень";
-            } else if ((hours == 24 || hours == 0) && minutes == 0) {
+            }
+            else if ((hours == 24 || hours == 0) && minutes == 0)
+            {
                 cout << "Полночь";
-            } else {
+            }
+            else
+            {
                 // обработка времени суток
-                if (hours == 24 || hours >= 0 && hours < 5) {
+                if (hours == 24 || hours >= 0 && hours < 5)
+                {
                     dayTimeEdit("ночи", dayTime);
-                } else if (hours >= 5 && hours < 12) {
+                }
+                else if (hours >= 5 && hours < 12)
+                {
                     dayTimeEdit("утра", dayTime);
-                } else if (hours >= 12 && hours < 18) {
+                }
+                else if (hours >= 12 && hours < 18)
+                {
                     dayTimeEdit("дня", dayTime);
-                } else {
+                }
+                else
+                {
                     dayTimeEdit("вечера", dayTime);
                 }
                 // перевод в 12-часовую систему
-                if (hours > 12) {
+                if (hours > 12)
+                {
                     hours -= 12;
                 }
                 // проверка формы часов
-                if (hours == 12) {
+                if (hours == 12)
+                {
                     cout << hours << " часов ";
-                } else if (hours % 10 == 1) {
+                }
+                else if (hours % 10 == 1)
+                {
                     cout << hours << " час ";
-                } else if (hours % 10 == 2 || hours % 10 == 3 || hours % 10 == 4) {
+                }
+                else if (hours % 10 == 2 || hours % 10 == 3 || hours % 10 == 4)
+                {
                     cout << hours << " часа ";
-                } else {
+                }
+                else
+                {
                     cout << hours << " часов ";
                 }
 
                 // проверка формы минут
 
-                if (minutes == 0) {
+                if (minutes == 0)
+                {
                     cout << dayTime << " ровно";
-                } else {
-                    if (minutes >= 10 && minutes <= 20) {
+                }
+                else
+                {
+                    if (minutes >= 10 && minutes <= 20)
+                    {
                         cout << minutes << " минут ";
-                    } else if (minutes % 10 == 1) {
+                    }
+                    else if (minutes % 10 == 1)
+                    {
                         cout << minutes << " минута ";
-                    } else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
+                    }
+                    else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4)
+                    {
                         cout << minutes << " минуты ";
-                    } else {
+                    }
+                    else
+                    {
                         cout << minutes << " минут ";
                     }
 
@@ -69,8 +106,8 @@ int main() {
                 }
             }
         }
-       
-    
+        cout << endl;
+    } while (true);
 
     return 0;
 }
